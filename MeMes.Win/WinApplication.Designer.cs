d9396d960b10a76281f322241d74bbe28edaed71
szx <1057095423@qq.com>
2020-12-23 14:38:59 +0800

    添加项目文件。

---
+++ MeMes.Win/WinApplication.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace MeMes.Win {
+    partial class MeMesWindowsFormsApplication {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.module1 = new DevExpress.ExpressApp.SystemModule.SystemModule();
+            this.module2 = new DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule();
+            this.module3 = new MeMes.Module.MeMesModule();
+            this.module4 = new MeMes.Module.Win.MeMesWindowsFormsModule();
+            this.objectsModule = new DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // MeMesWindowsFormsApplication
+            // 
+            this.ApplicationName = "MeMes";
+            this.CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+            this.Modules.Add(this.module1);
+            this.Modules.Add(this.module2);
+            this.Modules.Add(this.module3);
+            this.Modules.Add(this.module4);
+            this.Modules.Add(this.objectsModule);
+            this.UseOldTemplates = false;
+            this.DatabaseVersionMismatch += new System.EventHandler<DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs>(this.MeMesWindowsFormsApplication_DatabaseVersionMismatch);
+            this.CustomizeLanguagesList += new System.EventHandler<DevExpress.ExpressApp.CustomizeLanguagesListEventArgs>(this.MeMesWindowsFormsApplication_CustomizeLanguagesList);
+
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.SystemModule.SystemModule module1;
+        private DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule module2;
+        private MeMes.Module.MeMesModule module3;
+        private MeMes.Module.Win.MeMesWindowsFormsModule module4;
+        private DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule objectsModule;
+    }
+}
